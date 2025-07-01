b22b3daf0ac9ed9fea4f1c990f39d63006826a85
LokeshAshapu <lokeshashapu@gmail.com>
2025-07-01 19:52:18 +0530

    commit

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express =require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
+
 
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-app.cors(cors());
+app.use(cors());
 app.use(express.json());
-mongoose.connect('mongodb://localhost:27017/meditrack');
+// mongoose.connect("mongodb+srv://lokeshashapu:Loki_@506@cluster.mongodb.net/meditrack?retryWrites=true&w=majority");
+// mongoose.connect("mongodb://localhost:27017/meditrack", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+mongoose.connect('mongodb://localhost:27017/meditrack')
+    .then(() => console.log('✅ MongoDB connected'))
+    .catch(err => console.error('❌ MongoDB connection error:', err));
+
+
 
 const trackerSchema = new mongoose.Schema({
     email: { type: String, required: true },
