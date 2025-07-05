01d8b4016214acd29c01df8496a37d7f82d9a142
LokeshAshapu <lokeshashapu@gmail.com>
2025-07-05 20:55:41 +0530

    commit

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -55,7 +55,7 @@ function Dashboard() {
                 {message}
                 </div>
             )}
-        <h1 className="text-center mb-15 font-bold text-3xl text-indigo-700">Welcome dear subscriber..</h1>
+        <h1 className="text-center mb-15 font-bold text-3xl text-indigo-700">Welcome dear subscriber...</h1>
         <div className="mt-8 p-6 bg-white rounded-lg shadow-md mb-24">
             <h1 className="text-3xl font-bold mb-6 text-center text-indigo-700">
                 Your Tracks
