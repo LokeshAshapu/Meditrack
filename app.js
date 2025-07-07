133e0ef29e459b1015d3a36db890cccbbf5f7e62
LokeshAshapu <lokeshashapu@gmail.com>
2025-07-07 20:28:51 +0530

    commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
+const path = require("path");
 
 const app = express();
 app.use(cors());
