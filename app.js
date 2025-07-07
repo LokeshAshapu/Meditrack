9aa5d319d101bba7437202983ffce70dd4f362c7
LokeshAshapu <lokeshashapu@gmail.com>
2025-07-07 21:02:24 +0530

    commit

--- app.js
+++ app.js
@@ -165,8 +165,6 @@ cron.schedule('* * * * *', async () => {
     }
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server is running on port ${PORT}`);
